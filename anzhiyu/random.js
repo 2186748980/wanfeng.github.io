57059d873ee0af9059d687a41b53f73e1b149bff
2186748980 <z2186748980@gmail.com>
2026-01-08 22:58:48 +0800

    Site updated: 2026-01-08 22:58:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/15/6/","2024/08/15/如何自己搭建博客/"];function toRandomPost(){
+var posts=["2024/08/15/如何自己搭建博客/","2026/01/08/测试文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
